9a9c005d5c07ab3ffd36180864bcfa86017218ec
hasmituchil <hasmituchil@outlook.com>
2016-07-18 12:41:50 +0530

    Final waterjug

--- waterjug.py
+++ waterjug.py
@@ -1,32 +1,58 @@
-m = 0
-n = 0
-d = 0
-gcd = 0
-continue = 0
-def gcd(x, y):
-	for i==1 and i <= x and i <= y:
-		if (x%i == 0 and y%i ==0):
-			gcd = i
-			return i 
+from fractions import gcd
+
+cont = 0
+m = 1
+n = 1
+d = 1
 
 def redo():
-	m = input("Enter smaller bucket")
-	n = input("Enter larger bucket")
-	d = input("Enter desired water")
+	global m
+	global n
+	global d
+	m = int(input("Enter larger bucket"))
+	n = int(input("Enter smaller bucket"))
+	d = int(input("Enter desired water"))
+
 
 redo()
-s = gcd(m,n)
-if d % gcd == 0:
-	print("Great Inputs!")
-	continue = 1
 
-if continue == 0:
+s = gcd(m,n)
+
+
+
+if d % s == 0:
+	print("Great Inputs!")
+	cont = 1
+
+if cont == 0:
+	print("Hmmmmm.... Try harder")
 	redo()
 
-if continue == 1:
+
+if cont == 1:
 	k = 0
 	while k<m:
 		k+=n
-		print ("Add" +m+ "litres")
-	print("You have" + k + "litres")
+		print ('Add {} litres'.format(n))
+		if k == d:
+			print("Got desired water")
+		
+	print ('Have {} litres'.format(k))
+	k -= m
+	print ('Remove {} litres'.format(m))
+	if k == d:
+		print("Done")
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
